c054ac7118b1a809202f2270e3a1bae98a1a4e46
Geeta Ahirwar <geeta.ahirwar589@gmail.com>
2024-05-22 22:57:11 -0400

    first commit

---
+++ db/conn.mjs
@@ -0,0 +1,20 @@
+// // import { MongoClient } from "mongodb";
+// import mongoose from 'mongoose'; 
+
+
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// mongoose.connect(process.env.ATLAS_URI);
+// // const client = new MongoClient(process.env.ATLAS_URI);
+
+// let conn;
+// try {
+//   conn = await mongoose.connect();
+// } catch (e) {
+//   console.error(e);
+// }
+
+// let db = conn.db("sample_training");
+
+// export default db;
